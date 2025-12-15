cc619c8229ad150e3b18d29dc491731413da0308
Wojciech Władziński <wojtekwladzinski2003@gmail.com>
2025-12-15 19:34:10 +0100

    Endpoint continue train

---
+++ Makefile
@@ -0,0 +1,2 @@
+run-server:
+	uv run uvicorn src.main:app --host 0.0.0.0 --port 8080
---
+++ src/api/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+# class TrainInput(BaseModel):
+#     index: int
+#     title: str
+#     artist: str
+#     top_genre: str
+#     year: int
+#     beats_per_minute: int
+#     energy: int
+#     danceability: int
+#     loudness: int
+#     liveness: int
+#     valence: int
+#     length: int
+#     acousticness: int
+#     speechiness: int
+#     popularity: int
+
+
+class ContinueTraining(BaseModel):
+    model_name: str
+    train_input: list[str]
+    new_model_name: str
