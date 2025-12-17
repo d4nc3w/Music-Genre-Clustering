be65ec2716cef5b836143c17a66755a2dac7595a
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2025-12-17 21:39:11 +0100

    Fix endpoint naming for continue training

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from src.api.models import ContinueTraining
 from src.api.controller import continue_train_controller
 from src.api.models import PredictionInput
+from src.api.controller import predict_controller
 
 app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/continue-training")
+@app.post("/continue-train")
 async def continue_training(training_model: ContinueTraining):
     continue_train_controller(training_model)
     return {"message": "Model successfully created"}
