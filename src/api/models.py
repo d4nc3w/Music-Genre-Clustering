94367df360eebeea9f3dd4ef39512d2999aede84
Wojciech Wladzinski <wojtekwladzinski2003@gmail.com>
2025-12-23 18:54:26 +0100

    continue-train fix

--- src/api/models.py
+++ src/api/models.py
@@ -1,27 +1,26 @@
 from pydantic import BaseModel
 
-# class TrainInput(BaseModel):
-#     index: int
-#     title: str
-#     artist: str
-#     top_genre: str
-#     year: int
-#     beats_per_minute: int
-#     energy: int
-#     danceability: int
-#     loudness: int
-#     liveness: int
-#     valence: int
-#     length: int
-#     acousticness: int
-#     speechiness: int
-#     popularity: int
+class TrainInput(BaseModel):
+    index: int
+    title: str
+    artist: str
+    top_genre: str
+    year: int
+    beats_per_minute: int
+    energy: int
+    danceability: int
+    loudness: int
+    liveness: int
+    valence: int
+    length: int
+    acousticness: int
+    speechiness: int
+    popularity: int
 
-
+#only new_model_name because it is impossible to continue training on the KMeans
 class ContinueTraining(BaseModel):
-    model_name: str
-    train_input: list[str]
     new_model_name: str
+    train_input: list[TrainInput]
 
 class PredictionInput(BaseModel):
     model_name: str
