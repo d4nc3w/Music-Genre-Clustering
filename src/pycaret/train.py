cb36042f27649f1a103e76b894b40901075b62e7
Wojciech Wladzinski <wojtekwladzinski2003@gmail.com>
2026-01-02 11:40:22 +0100

    Added name parameter

--- src/pycaret/train.py
+++ src/pycaret/train.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 FEATURES = ["Beats Per Minute (BPM)", "Loudness (dB)", "Liveness",
             "Valence", "Acousticness", "Speechiness"]
 
-def train_model(number_of_clusters:int=3):
+def train_model(best_model_name: str, number_of_clusters:int=3):
     data = pd.read_csv(f"{BASE_DIR}/data/Spotify-2000.csv")
     if 'Index' in data.columns:
         data = data.drop('Index', axis=1)
@@ -20,11 +20,11 @@ def train_model(number_of_clusters:int=3):
     predictions = predict_model(kmeans, data = data.copy())
 
 
-    save_model(kmeans, f"{BASE_DIR}/models/best-model")
+    save_model(kmeans, f"{BASE_DIR}/models/{best_model_name}")
     return scoring, predictions
 
 #remove when cli application created
 if __name__ == '__main__':
-    s, p = train_model(5)
+    s, p = train_model("new_best_model")
     print(f"Scoring {s}")
     print(f"Predictions {p}")
